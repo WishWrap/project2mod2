a2f2bd81c8180cb02741a6a6898543b8a08af2b1
Marine ROSSI <marine.rossi11@gmail.com>
2023-11-21 14:16:34 +0100

    add button and fix bug

--- src/pages/FinalPage.jsx
+++ src/pages/FinalPage.jsx
@@ -1,35 +1,49 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function FinalPage () {
-const [data, Setdata] =useState([]); 
-useEffect(() => {
-alldata(); 
-},[]); 
-const alldata = () => {
-axios 
-.get("http://localhost:5005") // faire / avec what I want 
-.then ((response) => {
-    console.log(response.alldata)
-    Setdata(response.data)
-})
-.catch(error)
-console.log(error)
+function FinalPage() {
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    alldata();
+  }, []);
+  const alldata = () => {
+    axios
+      .get("http://localhost:5005/user") 
+      .then((response) => {
+        console.log(response.alldata);
+        setdata(response.data);
+      })
+      .catch(error);
+    console.log(error);
+  };
+  axios
+    .get("http://localhost:5005/measure") 
+    .then((response) => {
+      console.log(response.alldata);
+      setdata(response.data);
+    })
+    .catch(error);
+  console.log(error);
+
+  axios
+    .get("http://localhost:5005/whatIWant/") 
+    .then((response) => {
+      console.log(response.alldata);
+      setdata(response.data);
+    })
+    .catch(error);
+  console.log(error);
+
+  return (
+    <div>
+      <p>UserName + Measure + DataGift add a delete and update button</p>
+      <p>
+        {data.map((data) => (
+          <div></div>
+        ))}
+      </p>
+    </div>
+  );
 }
-return (
-    <div> 
-        <p>UserName + Measure + DataGift add a delete and update button</p>
-        <p>
-            {data.map((data)=>
-            (<div></div>
-            ))}
-            </p>
-            </div>
-   
-)
-    
-}
 
-
-
-export default FinalPage
+export default FinalPage;
